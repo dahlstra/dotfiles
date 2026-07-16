4ba25584711fe363e3e81dc5d40cba0ff698e0d7
Cory Dahlstrand <dahlstrand3@gmail.com>
2026-07-16 19:33:52 -0400

    Add better bufferline styling

--- AppData/Local/nvim/lua/themes.lua
+++ AppData/Local/nvim/lua/themes.lua
@@ -29,5 +29,5 @@ vim.cmd("colorscheme catppuccin")
 
 vim.o.background = "dark"
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
--- AppData/Local/nvim/plugin/bufferline.lua
+++ AppData/Local/nvim/plugin/bufferline.lua
@@ -3,4 +3,37 @@ vim.pack.add({
     "https://github.com/akinsho/bufferline.nvim",
 })
 
-require("bufferline").setup {}
+local mocha = require("catppuccin.palettes").get_palette("mocha")
+
+require("bufferline").setup({
+    highlights = require("catppuccin.special.bufferline").get_theme({
+        styles = { "bold" },
+
+        custom = {
+            mocha = {
+                buffer_selected = {
+                    bg = mocha.base,
+                    fg = mocha.text,
+                    bold = true,
+                },
+            },
+        },
+    }),
+
+    options = {
+        custom_filter = function(buf)
+            return vim.bo[buf].filetype ~= "oil"
+        end,
+        modified_icon = "*",
+        separator_style = "slant",
+        always_show_bufferline = true,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(_, level)
+            local icon = level:match("error") and " " or " "
+            return " " .. icon
+        end,
+        show_duplicate_prefix = true,
+    },
+})
